8083e65d91abb6502b6bb09f3d255df0a642d69c
Phil Tesar <philtesar@gmail.com>
2017-10-21 21:25:08 -0400

    First Commit

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('Whopper');
+INSERT INTO burgers (burger_name) VALUES ('Baconator');
+INSERT INTO burgers (burger_name) VALUES ('Tavern Double');
+INSERT INTO burgers (burger_name) VALUES ('Bonzai Burger');
+
+select * from burgers;
